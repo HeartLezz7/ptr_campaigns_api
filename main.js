c785c3dc7cc227827d8936f80bd2394a96983a89
HeartLezz7 <heartchantapath@gmail.com>
2024-01-23 21:37:30 +0700

    finish fix and percetage discount

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 const { readFile, writeFile } = require("./handleFile");
 
-const discount = async () => {
+function sumPrice(arr, category) {
+  return arr.reduce((acc, item) => {
+    if (item.category == category) {
+      acc += item.price;
+    }
+    return acc;
+  }, 0);
+}
+
+const discount = async (amout) => {
   try {
     let products = await readFile("products.json");
-    console.log(products);
+    let sum = 0;
+    let discount = 0;
+
+    const sumAccessory = sumPrice(products, "accessory");
+    const sumCloth = sumPrice(products, "cloth");
+    console.log(sumAccessory + sumCloth);
+
+    if (amout.fix != undefined) {
+      discount = amout.fix;
+      sum += sumAccessory + sumCloth - amout.fix;
+    }
+    if (amout.percentage != undefined) {
+      discount = 1 - amout.percentage / 100;
+      if (discount > 0 && discount < 1) {
+        sum += (sumAccessory + sumCloth) * discount;
+      } else {
+        sum += sumAccessory + sumCloth;
+      }
+    }
+
+    return sum;
   } catch (error) {
     console.log(error);
   }
 };
 
-discount();
+discount({ percentage: 10 }).then((res) => console.log(res));

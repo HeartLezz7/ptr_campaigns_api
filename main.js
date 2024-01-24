139703ad4c458410367afc22350781b2f02727cd
HeartLezz7 <heartchantapath@gmail.com>
2024-01-24 10:27:39 +0700

    finish discount with user point

--- main.js
+++ main.js
@@ -9,33 +9,49 @@ function sumPrice(arr, category) {
   }, 0);
 }
 
-const discount = async (amout) => {
+const discount = async (amout, isUse = false) => {
   try {
-    let products = await readFile("products.json");
-    let sum = 0;
+    const products = await readFile("products.json");
+    const user = await readFile("user.json");
+    let discountPrice = 0;
     let discount = 0;
 
     const sumAccessory = sumPrice(products, "accessory");
     const sumCloth = sumPrice(products, "cloth");
+    const totalPrice = sumAccessory + sumCloth;
     console.log(sumAccessory + sumCloth);
 
-    if (amout.fix != undefined) {
-      discount = amout.fix;
-      sum += sumAccessory + sumCloth - amout.fix;
-    }
-    if (amout.percentage != undefined) {
-      discount = 1 - amout.percentage / 100;
-      if (discount > 0 && discount < 1) {
-        sum += (sumAccessory + sumCloth) * discount;
-      } else {
-        sum += sumAccessory + sumCloth;
+    // coupon
+    if (amout.coupon != undefined) {
+      if (amout.coupon.fix != undefined) {
+        discount = amout.fix;
+        discountPrice += totalPrice - amout.fix;
+      } else if (amout.percentage != undefined) {
+        discount = 1 - amout.percentage / 100;
+        if (discount > 0 && discount < 1) {
+          discountPrice += totalPrice * discount;
+        }
       }
     }
 
-    return sum;
+    // on top
+    if (user.point > 0 && isUse) {
+      discount = user.point;
+      if (discount > totalPrice * 0.2) {
+        console.log("first");
+        discount = totalPrice * 0.2;
+        discountPrice += totalPrice - discount;
+      } else {
+        discountPrice += totalPrice - discount;
+      }
+    }
+
+    //seasonal
+
+    return discountPrice;
   } catch (error) {
     console.log(error);
   }
 };
 
-discount({ percentage: 10 }).then((res) => console.log(res));
+discount({ percentage: 10 }, true).then((res) => console.log(res));
